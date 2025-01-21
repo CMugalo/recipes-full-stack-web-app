10bd4048a182cfe261c88c3fef49de965e2e7f8e
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2025-01-21 08:25:15 +0300

    +delete route

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ api = Api(app, doc='/docs')
 
 recipe_model = api.model("Recipe", {
     "id": fields.Integer(),
-    "title": fields.String(),
+    "title": fields.String(), 
     "description": fields.String()
 })
 
@@ -62,8 +62,13 @@ class RecipeResource(Resource):
 
         return recipe_to_update
 
+    @api.marshal_with(recipe_model)
     def delete(self, id):
-        pass
+        recipe_to_delete = Recipe.query.get_or_404(id)
+
+        recipe_to_delete.delete()
+
+        return recipe_to_delete
 
 
 @app.shell_context_processor
